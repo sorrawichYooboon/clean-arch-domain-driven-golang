ad15b994c678aab130e5600abf6f7b419f80ede0
Sorrawich Yooboon (Ko) <sorrawich.y@gmail.com>
2024-09-19 22:44:19 +0700

    feat:try POST echo

--- main.go
+++ main.go
@@ -6,10 +6,27 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+type User struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
 func main() {
 	e := echo.New()
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+
+	e.POST("/users", func(c echo.Context) error {
+		user := new(User)
+
+		if err := c.Bind(user); err != nil {
+			return c.String(http.StatusBadRequest, "Invalid input")
+		}
+
+		return c.JSON(http.StatusOK, user)
+	})
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
